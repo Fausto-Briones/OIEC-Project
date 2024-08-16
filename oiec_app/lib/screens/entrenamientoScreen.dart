9e16ba498637861e2afbbb9dc046b24dd0d3d1c8
mbravop <mbravop03@gmail.com>
2024-08-16 13:30:01 -0500

    Adaptacion pantallas entrenador

--- oiec_app/lib/screens/entrenamientoScreen.dart
+++ oiec_app/lib/screens/entrenamientoScreen.dart
@@ -7,16 +7,16 @@ import 'package:oiec_app/globals.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 
 class EntrenamientoScreen extends StatefulWidget {
   const EntrenamientoScreen({super.key});
-  
+
   @override
   _EntrenamientoscreenState createState() => _EntrenamientoscreenState();
 }
 
 class _EntrenamientoscreenState extends State<EntrenamientoScreen> {
-  
   final Enrouter router = Enrouter.instance;
 
   @override
@@ -25,28 +25,29 @@ class _EntrenamientoscreenState extends State<EntrenamientoScreen> {
       title: "Entrenamientos",
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [ FutureBuilder<Map<String, Map<String, dynamic>>>(
-          future: DatabaseService.fetchTrainings(),
-          builder:(context, snapshot){
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              // Show a loading indicator while the data is being fetched
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              // Handle errors
-              return const Center(child: Text('Failed to load trainings.'));
-            } else if (snapshot.hasData) {
-              // Once data is loaded, display the tabs with contests
-              final entrenamientos = snapshot.data!['entrenamientos']!;
+        children: [
+          FutureBuilder<Map<String, Map<String, dynamic>>>(
+            future: DatabaseService.fetchTrainings(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // Show a loading indicator while the data is being fetched
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                // Handle errors
+                return const Center(child: Text('Failed to load trainings.'));
+              } else if (snapshot.hasData) {
+                // Once data is loaded, display the tabs with contests
+                final entrenamientos = snapshot.data!['entrenamientos']!;
 
-              return _Entrenamientos(entrenamientos);
-            } else {
-              // Handle the case where snapshot has no data
-              return const Center(child: Text('No hay entrenamientos disponibles.'));
-            }
-          },
-          
+                return _Entrenamientos(entrenamientos);
+              } else {
+                // Handle the case where snapshot has no data
+                return const Center(
+                    child: Text('No hay entrenamientos disponibles.'));
+              }
+            },
           ),
-          if (isTrainer)...[
+          if (isTrainer) ...[
             Padding(
               padding: EdgeInsets.only(
                   bottom: MediaQuery.of(context).size.height * 0.05),
@@ -65,9 +66,10 @@ class _EntrenamientoscreenState extends State<EntrenamientoScreen> {
 }
 
 class DatabaseService {
-  static final String baseUrl = 'https://dereck.pythonanywhere.com'; // Replace with your IP address
-  
-  static Future<Map<String,Map<String, dynamic>>> fetchTrainings() async {
+  static final String baseUrl =
+      'https://dereck.pythonanywhere.com/api'; // Replace with your IP address
+
+  static Future<Map<String, Map<String, dynamic>>> fetchTrainings() async {
     final response = await http.get(Uri.parse('$baseUrl/trainings'));
 
     if (response.statusCode == 200) {
@@ -88,36 +90,53 @@ class _Entrenamientos extends StatelessWidget {
   _Entrenamientos(this.entrenamientos);
 
   @override
-  Widget build(BuildContext context){
-    return entrenamientos.isEmpty 
-    ? const Center(
+  Widget build(BuildContext context) {
+    return entrenamientos.isEmpty
+        ? const Center(
             child: Text(
               'No hay entrenamientos disponibles.',
               style: TextStyle(color: Colors.white),
             ),
           )
-    : Column(
-            children: entrenamientos.values.map((entrenamiento){
+        : Column(
+            children: entrenamientos.values.map((entrenamiento) {
               return CustomContainer(
                 child: _EntrenamientoContent(
                   nombreActividad: entrenamiento['name'] ?? 'Unnamed Training',
-                  fecha: DateFormat.yMMMMd().format(DateTime.parse(entrenamiento['date'])),
+                  fecha: DateFormat.yMMMMd()
+                      .format(DateTime.parse(entrenamiento['date'])),
                   entrenador: entrenamiento['trainer'] ?? 'None',
+                  video: entrenamiento['video'],
+                  archivo: entrenamiento['files'],
                 ),
               );
             }).toList(),
           );
   }
-  
-  
 }
 
 class _EntrenamientoContent extends StatelessWidget {
   final String nombreActividad;
   final String fecha;
   final String entrenador;
-  const _EntrenamientoContent(
-      {required this.nombreActividad, required this.fecha, required this.entrenador});
+  final String video;
+  final String archivo;
+
+  const _EntrenamientoContent({
+    required this.nombreActividad,
+    required this.fecha,
+    required this.entrenador,
+    required this.video,
+    required this.archivo,
+  });
+
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'No se pudo abrir la URL: $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -126,13 +145,27 @@ class _EntrenamientoContent extends StatelessWidget {
       children: [
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [Text(nombreActividad), Text(fecha), Text(entrenador)],
+          children: [
+            Text(nombreActividad),
+            Text(fecha),
+            Text(entrenador),
+          ],
         ),
-        const Icon(
-          Icons.play_circle,
+        GestureDetector(
+          onTap: () => _launchURL(video),
+          child: Icon(
+            Icons.play_circle,
+            size: 30, // Tamaño del ícono
+            color: Colors.black, // Color del ícono
+          ),
         ),
-        const Icon(
-          Icons.picture_as_pdf,
+        GestureDetector(
+          onTap: () => _launchURL(archivo),
+          child: Icon(
+            Icons.picture_as_pdf,
+            size: 30, // Tamaño del ícono
+            color: Colors.black, // Color del ícono
+          ),
         ),
       ],
     );
--- oiec_app/lib/screens/trainmentFileScreen.dart
+++ oiec_app/lib/screens/trainmentFileScreen.dart
@@ -1,16 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:oiec_app/components/MainLayout.dart';
+import 'package:oiec_app/components/alertComponent.dart';
 import 'package:oiec_app/components/generalButtonComponent.dart';
 import 'package:oiec_app/components/inputTextComponent.dart';
 import 'package:oiec_app/services/navigator.dart';
+import 'package:oiec_app/globals.dart';
 
 class TrainmentFileScreen extends StatelessWidget {
   TrainmentFileScreen({super.key});
 
   final TextEditingController _controllerTitulo = TextEditingController();
-  final TextEditingController _controllerEnlace = TextEditingController();
+  final TextEditingController _controllerTaller = TextEditingController();
+  final TextEditingController _controllerEnlaceVideo = TextEditingController();
+  final TextEditingController _controllerEnlaceArchivos = TextEditingController();
 
   final Enrouter router = Enrouter.instance;
+
+  void agregarClase(String tituloTaller, String name, String video, String archivo) async {
+    CollectionReference user =
+        FirebaseFirestore.instance.collection('Trainings');
+
+    user.doc(tituloTaller).set({
+      'name': name,
+      'Team': "",
+      'trainer': cedula,
+      'date': Timestamp.now(),
+      'video': video,
+      'files': archivo
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +41,12 @@ class TrainmentFileScreen extends StatelessWidget {
           Column(
             children: [
               InputFieldWithLabel(
+                labelText: 'Título del taller',
+                controller: _controllerTaller,
+                hintText: '',
+                hintColor: Colors.grey,
+              ),
+              InputFieldWithLabel(
                 labelText: 'Título de la clase',
                 controller: _controllerTitulo,
                 hintText: '',
@@ -29,7 +54,13 @@ class TrainmentFileScreen extends StatelessWidget {
               ),
               InputFieldWithLabel(
                 labelText: 'Enlace de la clase',
-                controller: _controllerEnlace,
+                controller: _controllerEnlaceVideo,
+                hintText: '',
+                hintColor: Colors.grey,
+              ),
+              InputFieldWithLabel(
+                labelText: 'Enlace de archivos',
+                controller: _controllerEnlaceArchivos,
                 hintText: '',
                 hintColor: Colors.grey,
               ),
@@ -41,7 +72,19 @@ class TrainmentFileScreen extends StatelessWidget {
             child: GeneralButton(
               text: 'Agregar',
               onPressed: () {
-                router.navigateToEntrenamientosScreen(context);
+                var taller = _controllerTaller.text;
+                var clase = _controllerTitulo.text;
+                var video = _controllerEnlaceVideo.text;
+                var archivo = _controllerEnlaceArchivos.text;
+                if (taller.isEmpty || clase.isEmpty || video.isEmpty || archivo.isEmpty) {
+                  // ignore: use_build_context_synchronously
+                  showCustomAlertDialog(
+                      context, "Error", "Todos los campos son obligatorios.");
+                } else {
+                  agregarClase(taller, clase, video, archivo);
+                  
+                  router.navigateToEntrenamientosScreen(context);
+                }
               },
             ),
           )
